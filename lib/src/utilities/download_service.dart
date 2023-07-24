fcffebc5a87397ae0100b6277dc85c84eeb077b2
Kunduz <talaybekovna.kunduz@gmail.com>
2023-07-24 16:20:14 +0600

    Test web download.

--- lib/src/utilities/download_service.dart
+++ lib/src/utilities/download_service.dart
@@ -1,7 +1,10 @@
+import 'dart:io';
+
 import 'package:flutter/foundation.dart';
 import 'package:flutter_downloader/flutter_downloader.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:universal_html/html.dart' as html;
+import 'package:http/http.dart' as http;
 
 abstract class DownloadService {
   Future<String?> download({required String url});
@@ -18,7 +21,12 @@ abstract class DownloadService {
 class WebDownloadService implements DownloadService {
   @override
   Future<String?> download({required String url}) async {
-    html.window.open(url, "_blank");
+    // html.window.open(url, "_blank");
+    final response = await http.get(Uri.parse(url));
+    final byte = response.bodyBytes;
+    final File file = File('C:/downloads');
+    // await file.writeAsBytes(byte);
+    await file.writeAsBytes(Uint8List.fromList(byte));
     return 'success';
   }
 }
